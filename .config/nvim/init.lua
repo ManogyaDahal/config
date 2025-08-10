fa657143d9bddab71a25b44d554c8b0d9b955cb0
ManogyaDahal <bat16692@gmail.com>
2025-08-10 17:24:18 +0545

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,7 @@ vim.o.tabstop = 2
 vim.o.number = true
 vim.o.ignorecase = true
 vim.o.relativenumber = true
-vim.o.wrap = false -- wrap: prevents the text exceeding the width of the window
+vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.o.scrolloff = 0
@@ -15,6 +15,7 @@ vim.o.smartindent = true
 vim.o.winborder = "rounded"
 vim.o.incsearch = true
 vim.o.undofile = true
+vim.opt.colorcolumn = "80"
 
 -------Vim Keybinds----------
 vim.g.mapleader = " "
@@ -29,17 +30,20 @@ vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>v', ':e $MYVIMRC<CR>') --Look into this
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')--Look into this
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')--Look into this
 
 -----------Vim Plugins----------------
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/arnamak/stay-centered.nvim" },
 	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 
 require("nvim-web-devicons").setup()
@@ -73,3 +77,8 @@ vim.cmd [[
 --lsp
 vim.lsp.enable({ "lua_ls", "clang" })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+
+--snippets
+require("luasnip").setup({ enable_autosnippets = true })
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
