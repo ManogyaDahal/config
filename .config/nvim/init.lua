cc5edb697e2aa42bce6deb624054445ca7748cd0
ManogyaDahal <bat16692@gmail.com>
2025-08-10 10:19:53 +0545

    added mininmal setup for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,75 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+---------Vim Options-----------
+vim.o.clipboard = "unnamedplus"
+vim.o.hlsearch = false
+vim.o.tabstop = 2
+vim.o.number = true
+vim.o.ignorecase = true
+vim.o.relativenumber = true
+vim.o.wrap = false -- wrap: prevents the text exceeding the width of the window
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.o.scrolloff = 0
+vim.o.shiftwidth = 4
+vim.o.signcolumn = "yes"
+vim.o.smartindent = true
+vim.o.winborder = "rounded"
+vim.o.incsearch = true
+vim.o.undofile = true
+
+-------Vim Keybinds----------
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+vim.keymap.set('n', '<leader>so', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>x', ':quit<CR>')
 
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
+vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>fw', ':Pick grep_live<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
-vim.opt.rtp:prepend(lazypath)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { noremap = true, silent = true })
 
-local lazy_config = require "configs.lazy"
+-----------Vim Plugins----------------
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/arnamak/stay-centered.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
+require("nvim-web-devicons").setup()
+require("stay-centered").setup({})
+require("mini.pick").setup({})
+require("mason").setup({})
+require('nvim-tree').setup({
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	view = {
+		side = 'left',
+		width = 30,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true, -- auto close tree on file open
+		},
+	},
+})
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
 
-  { import = "plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "options"
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+--lsp
+vim.lsp.enable({ "lua_ls", "clang" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
