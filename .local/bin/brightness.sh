db89944a5bab4d8fc548172c041edda7a9577e6b
ManogyaDahal <bat16692@gmail.com>
2025-09-13 21:18:18 +0545

    update

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 function send_notification() {
-	brightness=$(brightnessctl get)
+    current=$(brightnessctl g)
+    max=$(brightnessctl m)
+    percent=$(( 100 * current / max ))
 
-	dunstify -a "changebrightness" -u low -r "9993" -h int:value:"$brightness" "Brightness ${brightness}" -t 2000
+    dunstify -a "changebrightness" \
+        -u low \
+        -r "9993" \
+        -h int:value:"$percent" \
+        "Brightness ${percent}%" \
+        -t 2000
 }
 
 case $1 in
 up)
-  brightnessctl -e set 5%+
-	send_notification $1
-	;;
+    brightnessctl -e set 5%+
+    send_notification
+    ;;
 down)
-  brightnessctl -e set 5%-
-	send_notification $1
-	;;
+    brightnessctl -e set 5%-
+    send_notification
+    ;;
 esac
