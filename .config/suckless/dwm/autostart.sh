33b142f967ee68948bbcfdb72afea1b8fd416e66
ManogyaDahal <bat16692@gmail.com>
2025-12-17 16:38:51 +0545

    removed bar

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
-
-# Go to tag 1
-xdotool key super+1
-
-# Toggle bar
-xdotool key super+b
-
-# Switch to monocle
-xdotool key super+m
+# #!/bin/sh
+#
+# # Go to tag 1
+# xdotool key super+1
+#
+# # Toggle bar
+# xdotool key super+b
+#
+# # Switch to monocle
+# xdotool key super+m
+#
+#
+# #!/bin/bash
+#
+# # Wait for dwm to initialize before sending the signal to hide the bar
+# sleep 0.5
+# pkill -USR1 dwm   # This sends the signal to hide the bar
--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -3,14 +3,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char dmenufont[] = "Hack Nerd Font:size=10";
 static const char *fonts[] = {"Hack Nerd Font:style:medium:size=10", "Hack Nerd Font:style:medium:size=10", "Symbols Nerd Font:size=9"};
@@ -18,10 +18,10 @@ static const char col_gray1[] = "#222222"; // backgroud colour normally seen
 static const char col_gray2[] = "#E8B082"; // border for unfocused window
 static const char col_gray3[] = "#fff";    // font color ;normal
 static const char col_gray4[] = "#000000"; // font color when focused on a window
-// static const char col_cyan[] = "#E8B082";  // border color when the window is focused
+static const char col_cyan[] = "#E8B082";  // border color when the window is focused
 // static const char col_cyan[] = "#83bae8";  // border color when the window is focused
 static const char col_active[] = "#83bae8";
-static const char col_cyan[] = "#ABE9B3";  // border color when the window is focused
+// static const char col_cyan[] = "#ABE9B3";  // border color when the window is focused
                                            //this is the default color #83bae8
 
 static const char *colors[][3] = {
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class          instance    title             tags mask     isfloating   monitor */
-   {"zen",       NULL,       NULL,         		1 << 3,         0,           -1},
+   {"firefox",       NULL,       NULL,         		1 << 3,         0,           -1},
    {"TelegramDesktop", NULL,       NULL,         1 << 2,         0,           -1 },
    {"discord",         NULL,       NULL,         1 << 2,         0,           -1 },
    {"com.github.th_ch.youtube_music",         NULL,       NULL,         1 << 1,         0,           -1 },
@@ -101,8 +101,7 @@ static const Key keys[] = {
   {0,           F6,               spawn, SHCMD("~/.local/bin/brightness.sh up")},
   {0,           F5,               spawn, SHCMD("~/.local/bin/brightness.sh down")},
   {0,           F4,               spawn, SHCMD("alacritty -e ranger ~/School")},
-  {MODKEY,      XK_b,             spawn, SHCMD("zen-browser --private-window --new-tab")},//new tab for private window
-  {MODKEY,      XK_p,             spawn, SHCMD("zen-browser --private-window --new-tab")},//new tab for private window
+  {MODKEY,      XK_p,             spawn, SHCMD("firefox --private-window --new-tab")},//new tab for private window
   // custom keybindings start {end}
 
 	/* modifier                     key        function        argument */
